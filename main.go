29f1ac099b9df5ed8727d5c78754e57220904609
Marcos Paulo de Souza Brito <marcospsbrito@gmail.com>
2017-07-19 16:58:47 -0300

    change de logic of select piece. Now you can type de number of piece and select a moviment avaliable to do. It's using map structure to save the coordenates of each piece.

--- main.go
+++ main.go
@@ -3,15 +3,52 @@ package main
 import 	"fmt"
 
 const (
+	DOWN  = 1
 	UP    = 2
 	LEFT  = 3
 	RIGHT = 4
-	DOWN  = 1
+	EMPTY = "E"
+	DENIED = "D"
+	NOT_AVALIABLE = "NA"
 )
 
+type slot struct {
+	x,y int
+	slotType string
+	piece *piece
+}
+
+func (s slot) isEmpty() bool{
+	return s.slotType == EMPTY
+}
+
+func (s slot) hasPiece() bool{
+	return s.slotType == NOT_AVALIABLE
+}
+func (s *slot) setPiece(piece *piece){
+	s.piece = piece
+	s.piece.setSlot(s)
+	s.slotType=NOT_AVALIABLE
+}
+func (piece *piece) setSlot(slot *slot) {
+	piece.s = slot
+}
+
+func (s *slot) free() {
+	if s.slotType == NOT_AVALIABLE{
+		*s = slot{s.x, s.y, EMPTY, nil}
+	}
+}
+
+type piece struct {
+	key int
+	s *slot
+}
+
 type tabuleiro struct {
-	pieces       [9][9]int
-	originCoord,destinyCoord  [2]int
+	slots [9][9]*slot
+	piecesMap map[int]*piece
+	pieceSelected int
 	movement int
 }
 
@@ -24,23 +61,26 @@ func (t *tabuleiro) start() {
 
 func (t *tabuleiro)  init(){
 	key :=1
+	t.piecesMap = make(map[int]*piece)
 	for i:=0; i<9;i++{
 		for j:=0; j<9;j++{
+				slot := new(slot)
 			if t.coordsValid(i,j) {
-				t.pieces[i][j] = key
+				piece := piece{key, slot}
+				slot.x,slot.y,slot.slotType,slot.piece = i,j,NOT_AVALIABLE,&piece
+				t.piecesMap[key] = &piece
 				key++
 			}else{
-				t.pieces[i][j] = -1
+				slot.x,slot.y,slot.slotType,slot.piece = i,j,DENIED, nil
 			}
+			t.slots[i][j] = slot
 		}
 	}
-	t.pieces[4][4] = 0
+	t.slots[4][4].free()
 }
 
 func (t *tabuleiro) selectOriginSlot(){
-	fmt.Println("Enter de coordenates of the piece.")
-	t.originCoord = t.askUserCoordenate()
-
+	t.askPieceNumber()
 }
 func (t *tabuleiro) selectDestinySlot(){
 	t.movement = 0
@@ -61,33 +101,33 @@ func (t *tabuleiro) askMovement(){
 		if t.canOriginMove(RIGHT){
 			fmt.Println("4) Right" )
 		}
-		fmt.Print("Type the number of movement \n")
+		fmt.Print("Type the movement number: ")
 		fmt.Scanf("%d", &t.movement)
-		if !t.canMove(t.originCoord[0], t.originCoord[1], t.movement) {
+		if !t.canMove(t.pieceSelected, t.movement) {
 			t.movement = 0
 		}
 	}
 	fmt.Printf("Selected movement %d \n", t.movement)
 }
 
-func (t tabuleiro) askUserCoordenate() ([2]int) {
-	var coordX, coordY =-1,-1
+func (t *tabuleiro) askPieceNumber() {
+	t.pieceSelected = -1
 
-	for !isCoordValid(coordX) {
-		fmt.Print("Enter the column: ")
-		fmt.Scanf("%d", &coordX)
+	for valid := false; !valid; valid = t.isPieceValid(t.pieceSelected) {
+		fmt.Print("Enter the Piece Number: ")
+		fmt.Scanf("%d", &t.pieceSelected)
 	}
 
-	for !isCoordValid(coordY) {
-		fmt.Print("Enter the line: ")
-		fmt.Scanf("%d", &coordY)
-	}
-	fmt.Printf("Selected (%d, %d)\n",coordX,coordY)
-	return [2]int{coordX,coordY}
+	fmt.Printf("Piece selected (%d)\n",t.pieceSelected)
 }
-
-func isCoordValid(coord int) bool {
-	return coord >= 0 && coord < 9
+func (t tabuleiro) isPieceValid(pieceNumber int) bool {
+	if val, ok := t.piecesMap[pieceNumber]; ok{
+		if &val != nil && !val.s.isEmpty(){
+			return true
+		}
+	}
+	fmt.Println("Invalid pieceNumber ", pieceNumber)
+	return false
 }
 
 func (t *tabuleiro) doMovement() {
@@ -103,44 +143,46 @@ func (t *tabuleiro) doMovement() {
 			t.selectDestinySlot()
 			destinyValid = t.isDestinyValid()
 		}
-		x, y := t.originCoord[0], t.originCoord[1]
-		slotToMove, slotToFree := [2]int{x, y},[2]int{x, y}
-		switch move := t.movement; move{
-		case 1:
-			slotToMove[0] = slotToMove[0]+2
-			slotToFree[0] = slotToFree[0]+1
-		case 2:
-			slotToMove[0] = slotToMove[0]-2
-			slotToFree[0] = slotToFree[0]-1
-		case 3:
-			slotToMove[1] = slotToMove[1]-2
-			slotToFree[1] = slotToFree[1]-1
-		case 4:
-			slotToMove[1] = slotToMove[1]+2
-			slotToFree[1] = slotToFree[1]+1
-		default:
-			fmt.Println("Invalid movement! - ", move)
-		}
-		t.pieces[slotToMove[0]][slotToMove[1]] = t.pieces[t.originCoord[0]][t.originCoord[1]]
-		t.pieces[slotToFree[0]][slotToFree[1]] = 0
-		t.pieces[t.originCoord[0]][t.originCoord[1]] = 0
+		t.moveSelectedPiece()
 		t.movement=0
 		fmt.Println("Moviment done!")
 	}
 
 }
+func (t *tabuleiro) moveSelectedPiece() {
+	s := t.getPiece(t.pieceSelected)
+	switch move := t.movement; move{
+	case UP:
+		t.move(s, t.slots[s.x-2][s.y])
+	case DOWN:
+		t.move(s, t.slots[s.x+2][s.y])
+	case LEFT:
+		t.move(s, t.slots[s.x][s.y-2])
+	case RIGHT:
+		t.move(s, t.slots[s.x][s.y+2])
+	default:
+		fmt.Println("Invalid movement! - ", move)
+	}
+}
+func (t *tabuleiro) move(origin *slot, destiny *slot) {
+	slotToFree := t.slots[origin.x+((destiny.x-origin.x)/2)][origin.y+((destiny.y-origin.y)/2)]
+	destiny.setPiece(origin.piece)
+	slotToFree.free()
+	origin.free()
+}
 func (t *tabuleiro) printPieces() {
 	fmt.Println("\\\\\tY\t0\t1\t2\t3\t4\t5\t6\t7\t8\t|")
 	fmt.Println("X \t\t__\t__\t__\t__\t__\t__\t__\t__\t__\t")
-	for x, line := range t.pieces{
+	for x, line := range t.slots{
 		fmt.Print(x,"\t|\t")
-		for _, piece := range line{
-			if piece == -1 {
+		for _, slot := range line{
+			switch slot.slotType {
+			case DENIED:
 				fmt.Print("++\t")
-			}else if piece == 0 {
+			case NOT_AVALIABLE:
+				fmt.Print(slot.piece.key,"\t")
+			case EMPTY:
 				fmt.Print("[]\t")
-			}else{
-				fmt.Print(piece,"\t")
 			}
 		}
 		fmt.Print("|\n")
@@ -158,42 +200,49 @@ func (t *tabuleiro) hasMovements() bool{
 	return false
 }
 func (t *tabuleiro) hasMovement(x int, y int) bool{
-	return !t.slotFree([2]int{x, y}) && t.canMove(x, y,1) || t.canMove(x, y, 2) || t.canMove(x, y,3) || t.canMove(x , y,4)
+	if t.slots[x][y].piece==nil{
+		return false
+	}
+	pk :=t.slots[x][y].piece.key
+	return !t.slotFree([2]int{x, y}) && t.canMove(pk,1) ||
+		t.canMove(pk, 2) || t.canMove(pk,3) || t.canMove(pk,4)
 }
 func (t *tabuleiro) isOriginValid() bool{
-	valid := t.hasMovement(t.originCoord[0], t.originCoord[1]) && t.getPiece(t.originCoord[0], t.originCoord[1]) > 0
+	slot := t.getPiece(t.pieceSelected)
+	valid := t.hasMovement(slot.x, slot.y) && slot.hasPiece()
 	if !valid  {
-		fmt.Printf("Slot (%d, %d) is invalid.\n", t.originCoord[0], t.originCoord[1])
+		fmt.Printf("Piece (%d) can't be moved.\n", t.pieceSelected)
 	}
 	return valid
 }
 func (t *tabuleiro) canOriginMove(direction int) bool{
-	return t.canMove(t.originCoord[0], t.originCoord[1],direction)
+	return t.canMove(t.pieceSelected, direction)
 }
-func (t *tabuleiro) canMove(x int, y int, direction int) bool{
-	if direction <1 || direction > 4{
+func (t *tabuleiro) canMove(pieceNumber int, direction int) bool{
+	if direction <1 || direction > 4 || t.getPiece(pieceNumber) == nil {
+		fmt.Println("no piece found - ", pieceNumber)
 		return false
 	}
-	moves := [4][2]int{{x+2,y},{x-2,y},{x,y-2},{x,y+2}}
-	piecesToEat := [4][2]int{{x+1,y},{x-1,y},{x,y-1},{x,y+1}}
-	if t.coordsValid(x,y) && !t.slotFree([2]int{x, y}) && t.slotFree(moves[direction-1]) && !t.slotFree(piecesToEat[direction-1]){
+	s := t.getPiece(pieceNumber)
+	moves := [4][2]int{{s.x+2,s.y},{s.x-2,s.y},{s.x,s.y-2},{s.x,s.y+2}}
+	piecesToEat := [4][2]int{{s.x+1,s.y},{s.x-1,s.y},{s.x,s.y-1},{s.x,s.y+1}}
+	if t.coordsValid(s.x,s.y) && s.hasPiece() && t.slotFree(moves[direction-1]) && !t.slotFree(piecesToEat[direction-1]){
 			return true
 	}
 	return false
 }
 func (t *tabuleiro) slotFree(coords [2]int) bool {
 	if t.coordsValid(coords[0],coords[1]){
-		return  t.getPiece(coords[0],coords[1])==0
+		return  t.slots[coords[0]][coords[1]].isEmpty()
 	}
 	return false
 }
 func (t *tabuleiro) coordsValid(i,j int) bool{
 	return (i>2 && i<6 && j>=0 && j<9) || (j>2 && j<6 && i>=0 && i<9)
-	//return (i>2 && i<6 && j>=3 && j<6) || (j>2 && j<6 && i>=3 && i<6)
 }
 
 func (t *tabuleiro) isDestinyValid() bool{
-	coords := t.originCoord
+	coords := [2]int{t.getPiece(t.pieceSelected).x,t.getPiece(t.pieceSelected).y}
 	switch move := t.movement; move{
 	case DOWN:
 		coords[0] = coords[0]+2
@@ -206,14 +255,14 @@ func (t *tabuleiro) isDestinyValid() bool{
 	default:
 		fmt.Println("Invalid movement! - ", move)
 	}
-	if t.coordsValid(coords[0],coords[1]) && t.getPiece(coords[0], coords[1]) == 0 {
+	if t.coordsValid(coords[0],coords[1]) && t.slots[coords[0]][coords[1]].isEmpty() {
 		return true
 	}
 	return false
 }
 
-func (t *tabuleiro) getPiece(coordX, coordY int) int{
-	return t.pieces[coordX][coordY]
+func (t *tabuleiro) getPiece(pieceNumber int) *slot{
+	return t.piecesMap[pieceNumber].s
 }
 
 func main() {
